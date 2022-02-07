5e24b3fe23689bf7e13c994099adf8614421a75e
Shamay Bahar <96354508+shamayun@users.noreply.github.com>
2022-02-06 21:36:21 -0500

    Update Schema.sql

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Schema.sql
@@ -36,8 +36,8 @@ CREATE TABLE titles (
   title varchar NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  --PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
@@ -45,8 +45,11 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+  --PRIMARY KEY (emp_no)
 );
-
 --SELECT * FROM departments;
+--SELECT * FROM dept_emp;
+--SELECT * FROM dept_manager;
+--SELECT * FROM salaries;
+--drop table dept_emp;
